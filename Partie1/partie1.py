4bba8ee1686dcae875f99363e6f632de7527d306
cherif <simocherif2001@gmail.com>
2022-11-05 00:23:55 +0100

    second

--- Partie1/partie1.py
+++ Partie1/partie1.py
@@ -21,23 +21,48 @@ def EncodeText(Text: str) -> str:
     taille_bin = taille_bin[2:]
     while len(taille_bin) < 16 :
         taille_bin = "0" + taille_bin
-
     return txt_binaire + taille_bin 
+
+def InsertText(Text : str, imgBShape):
+    
+    font = cv.FONT_HERSHEY_SIMPLEX
+    org = (50, 150)
+    fontScale = 2
+    color = (0, 0, 0)
+    thickness = 3
+    image = np.ones(imgBShape, dtype=np.uint8)
+    
+    # Using cv2.putText() method
+    image = cv.putText(image, 'OpenCV', org, font, 
+                    fontScale, color, thickness, cv.LINE_AA)
+
+    
+    return image   
+
+
 
 
 
 def Encode(text, ImagePath):
 
-    image = cv.read(ImagePath)
+    image = cv.imread(ImagePath, cv.IMREAD_COLOR)
 
-    d1,d2,d3 = image.shape 
+    img_ycbcr = cv.cvtColor(image, cv.COLOR_BGR2YCR_CB)
 
-    for y in range(d2) : 
-        for x in range(d1):
-            for z in range(d3):
+    imgB = InsertText(text, ImagePath)
 
-                print("hey")
+    d1,d2,d3 = img_ycbcr.shape 
 
+    for y in range(d1) : 
+        for x in range(d2):
+            #for z in range(d3):
+
+            px = img_ycbcr[x,y,0]
+            bin_ = list(bin(px))[2:]
+            print(bin_)
+            #img_ycbcr[x,y,0] = bin_[]
+            break
+        break
 
     return 0
 
@@ -47,3 +72,49 @@ def DecodeText(Txt_Binaire : str ) -> str :
 
 
 print(EncodeText("Hello"))
+
+
+image = cv.imread("img.jpg",cv.IMREAD_COLOR)
+
+img2 = cv.cvtColor(image, cv.COLOR_BGR2YCR_CB)
+
+print(img2.shape)
+
+print(img2[:,:,0].min())
+
+"""
+cv.imshow("GRAY2",img2)
+cv.waitKey(0)
+cv.destroyAllWindows()
+"""
+
+#Encode("Hello","img.jpg")
+
+# Window name in which image is displayed
+window_name = 'Image'
+  
+# font
+font = cv.FONT_HERSHEY_SIMPLEX
+  
+# org
+org = (50, 150)
+  
+# fontScale
+fontScale = 2
+   
+# Blue color in BGR
+color = (0, 0, 0)
+  
+# Line thickness of 2 px
+thickness = 3
+   
+
+image = np.ones(img2.shape)
+# Using cv2.putText() method
+image = cv.putText(image, 'OpenCV', org, font, 
+                   fontScale, color, thickness, cv.LINE_AA)
+   
+# Displaying the image
+cv.imshow(window_name, image) 
+cv.waitKey(0)
+cv.destroyAllWindows()
