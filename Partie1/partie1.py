c3ea14693cd1d27660bef5517c503c1feeac04da
cherif <simocherif2001@gmail.com>
2022-11-05 00:49:18 +0100

    second

--- Partie1/partie1.py
+++ Partie1/partie1.py
@@ -30,14 +30,33 @@ def InsertText(Text : str, imgBShape):
     fontScale = 2
     color = (0, 0, 0)
     thickness = 3
+    txt_binaire = ""
+    taille_bin = ""
+
     image = np.ones(imgBShape, dtype=np.uint8)
     
     # Using cv2.putText() method
     image = cv.putText(image, 'OpenCV', org, font, 
                     fontScale, color, thickness, cv.LINE_AA)
 
+    imgB = cv.cvtColor(image,cv.COLOR_BGR2GRAY)
+
+    for y in range(image.shape[0]):
+        for x in range(image.shape[1]):
+            bin_rep = bin(imgB[x,y]) #transformer en binaire
+            bin_rep = bin_rep [2:] #enlever '0b' au debur
+
+        while len(bin_rep) < 8 : #pour avoir a la fin chaque lettre sur 1 octet
+            bin_rep = "0" + bin_rep   
+
+        txt_binaire += bin_rep
     
-    return image   
+    #calculer la taille qui va etre representée sur 2 octets (16 bits)
+    taille_bin = bin(len(txt_binaire))
+    taille_bin = taille_bin[2:]
+    while len(taille_bin) < 16 :
+        taille_bin = "0" + taille_bin
+    return txt_binaire + taille_bin 
 
 
 
@@ -49,22 +68,33 @@ def Encode(text, ImagePath):
 
     img_ycbcr = cv.cvtColor(image, cv.COLOR_BGR2YCR_CB)
 
-    imgB = InsertText(text, ImagePath)
+    codage = InsertText(text, ImagePath)
 
     d1,d2,d3 = img_ycbcr.shape 
-
+    index = 0 
     for y in range(d1) : 
         for x in range(d2):
-            #for z in range(d3):
+            for z in range(d3-1):
 
-            px = img_ycbcr[x,y,0]
-            bin_ = list(bin(px))[2:]
-            print(bin_)
-            #img_ycbcr[x,y,0] = bin_[]
-            break
-        break
+                px = img_ycbcr[x,y,z+1]
+                bin_ = list(bin(px))[2:]
 
-    return 0
+                bin_[4] = codage[index]
+                
+                index += 1
+                bin_[5] = codage[index]
+
+                index += 1
+                img_ycbcr[x,y,z+1] = int("".join(bin_),2)
+
+                if len(codage) <= index :
+                    break
+            if len(codage) <= index :
+                    break
+        if len(codage) <= index :
+                    break
+
+    return cv.cvtColor(img_ycbcr, cv.COLOR_YCR_CB2BGR)
 
 
 def DecodeText(Txt_Binaire : str ) -> str :
@@ -89,32 +119,3 @@ cv.destroyAllWindows()
 """
 
 #Encode("Hello","img.jpg")
-
-# Window name in which image is displayed
-window_name = 'Image'
-  
-# font
-font = cv.FONT_HERSHEY_SIMPLEX
-  
-# org
-org = (50, 150)
-  
-# fontScale
-fontScale = 2
-   
-# Blue color in BGR
-color = (0, 0, 0)
-  
-# Line thickness of 2 px
-thickness = 3
-   
-
-image = np.ones(img2.shape)
-# Using cv2.putText() method
-image = cv.putText(image, 'OpenCV', org, font, 
-                   fontScale, color, thickness, cv.LINE_AA)
-   
-# Displaying the image
-cv.imshow(window_name, image) 
-cv.waitKey(0)
-cv.destroyAllWindows()
